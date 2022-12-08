9e21589ee7635fc57f329bb96d770d80a84c562b
Yash Patel <94280370+yashpatel08@users.noreply.github.com>
2022-12-08 14:29:20 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+
+        function myfunction()
+        {
+         var x=document.getElementById("navbar");
+          if  (x.className==="navbar")
+          {
+            x.className += "responsive";
+          }
+          else
+          {
+            x.className = "navbar";
+          }
+
+        }
+    
+
