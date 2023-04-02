26f7308f1b1397c1d33b324be1c371a6f4858c61
Yash <yashpatel54257@gmail.com>
2023-04-02 21:49:35 +0530

    updated

--- portfolio.js
+++ portfolio.js
@@ -1,24 +1,45 @@
-  
-        function myfunction()
-        {
-         var x=document.getElementById("navbar");
-          if  (x.className==="navbar")
-          {
-            x.className += "responsive";
-          }
-          else
-          {
-            x.className = "navbar";
-          }
+function changeClass() {
+   
+      const element = document.getElementById("toggle");
+      if (element.className == "toggle_container") {
+         element.className = "toggle_container-open";
+      } else {
+         element.className = "toggle_container";
+      }
+   }
 
-        }
-    
+// function classToggle(){
+//    const nav=document.querySelectorAll('.toggle_container');
 
-        function myFunction() {
-          var x = document.getElementById("alllinks");
-          if (x.style.display === "block") {
-            x.style.display = "none";
-          } else {
-            x.style.display = "block";
-          }
-        }
+//    nav.forEach(nav => nav.classList.toggle('.toggle-items'));
+
+// }
+
+// document.querySelector('.toggle_btn').addEventListener('click',classToggle); 
+
+
+// const toggleBtn = document.querySelector('.toggle_btn');
+// const toggleBtnIcon = document.querySelector('.toggle_btn i');
+// const toggle_containerr = document.querySelector('.toggle_container');
+
+// toggleBtn.onclick = function () {
+//   toggle_containerr.classList.toggle('open');
+
+//   const isOpen = dropDownMenu.classList.contains('open');
+
+//   toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+// }
+
+
+
+// /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+// function openNav() {
+//    document.getElementById("toggle").style.width = "220px";
+//    document.getElementById("btn").style.marginLeft = "220px";
+//  }
+ 
+//  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+//  function closeNav() {
+//    document.getElementById("toggle").style.width = "0";
+//    document.getElementById("btn  ").style.marginLeft = "0";
+//  }
